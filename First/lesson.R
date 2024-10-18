b384ed3080db745e1dd2d7b384b81f9b4890e62f
unknown <User@c-075-5.rgmu.ru>
2024-10-18 19:41:55 +0300

    Email

--- First/lesson.R
+++ First/lesson.R
@@ -1,10 +1,8 @@
+num1 <- as.numeric(readline(prompt="чисто1")) #14
 
 
-num1 <- as.numeric(readline(prompt="чисто1"))
-
-
-num2_str <- readline(prompt="число2 ")
-num2_comma <- as.numeric(gsub(",", ".", num2_str))
+num2_str <- readline(prompt="число2 ") #15,8
+num2_comma <- as.numeric(gsub(",", ".", num2_str)) #convert to int
 
 sum <- num1 + num2_comma
 print(paste("sum is:", sum))
@@ -18,3 +16,4 @@ transition<-function(x){
 example<-transition(sum)
 
 
+#zhuravleva_si@mail.ru
